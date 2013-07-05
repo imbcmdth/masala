f856914fc57e8619bc38b6f70e75fe110f4f15d9
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2013-07-04 22:56:25 -0400

    crazy changes

--- masala.js
+++ masala.js
@@ -8,45 +8,82 @@
 		}
 	}(this, function () {
 		"use strict";
+		var slice = Array.prototype.slice;
+		var call = Function.prototype.call;
+		var toString = Object.prototype.toString;
 
-		var not = function(fn){ return function(){ return !fn.apply(this, arguments); }; };
+		var toArray = function () { return call.apply(slice, arguments); }
 
-		var nullOrUndefined = function(e){ return this[e] == null; };
+		var not = function (fn) { return function() { return !fn.apply(this, arguments); }; };
+
+		var nullOrUndefined = function(e) { return this[e] == null; };
 		var notNullOrUndefined = not(nullOrUndefined);
 
-		var existsIn = function(e){ return this.indexOf(e) != -1; };
+		var existsIn = function (e) { return this.indexOf(e) != -1; };
 		var doesntExistIn = not(existsIn);
 
-		var merge = function(dest, source){
+		var merge = function (dest, source) {
 			var validKeys = Object.keys(source).filter(notNullOrUndefined, source);
 
-			validKeys.forEach(function(e){
+			validKeys.forEach(function (e) {
 				dest[e] = source[e];
 			});
 
 			return dest;
 		};
 
-		var genSauce = function(fn, existingArgs, argsRemaining){
-			return function(args){
-				var argsGiven = Object.keys(args).filter(notNullOrUndefined, args),
-				    nextArgsRemaining = argsRemaining.filter(doesntExistIn, argsGiven);
+		var theTypeOf = function (thing) {
+			var type = toString.call(thing);
+			return type.toLowerCase().match(/^\[object (.+)\]$/)[1];
+		};
 
-				// We use `Object.create` to make a new `existingArgs` object
-				// so that each masala'd function is pure
-				existingArgs = merge(Object.create(existingArgs), args);
+		var genSauce = function (fn, optsPosition, existingOpts, optsRemaining, args) {
+			return function (opts) {
+				var argsOffset = +(theTypeOf(opts) === "object");
 
-				if ( nextArgsRemaining.length === 0 ) return fn.call(this, existingArgs);
+				// A) Merge remaining arguments
+				var nextArgs = args.concat(toArray(arguments, argsOffset)),
+				    fnLength = fn.length - 1; // remove options-object from length
 
-				return genSauce(fn, existingArgs, nextArgsRemaining);
+				if ( argsOffset ) {
+					// B) Merge options arguments
+					var optsGiven = Object.keys(opts).filter(notNullOrUndefined, opts);
+					var nextOptsRemaining = optsRemaining.filter(doesntExistIn, optsGiven);
+
+					// We use `Object.create` to make a new `existingOpts` object
+					// so that each masala'd function is pure
+					existingOpts = merge(Object.create(existingOpts), opts);
+				} else {
+					var nextOptsRemaining = optsRemaining;
+				}
+
+				// If the stars have aligned we apply the result
+				if ( nextOptsRemaining.length === 0 && nextArgs.length >= fnLength ) {
+					if ( nextArgs.length > fnLength ) nextArgs.splice(fnLength);
+
+					// Stick the options object back into the right place
+					nextArgs.splice(optsPosition, 0, existingOpts);
+
+					return fn.apply(this, nextArgs);
+				}
+
+				return genSauce(fn, optsPosition, existingOpts, nextOptsRemaining, nextArgs);
 			};
 		};
 
-		var masala = function(fn, args){
-			var argsRemaining = Object.keys(args).filter(nullOrUndefined, args),
-			    defaultArgs = merge({}, args);
+		var masala = function (fn, optsPosition, opts) {
+			var args = toArray(arguments, 3);
 
-			return genSauce(fn, defaultArgs, argsRemaining);
+			if ( theTypeOf(optsPosition) === "object" ) {
+				opts = optsPosition;
+				optsPosition = 0;
+				args = toArray(arguments, 2);
+			}
+
+			var optsRemaining = Object.keys(opts).filter(nullOrUndefined, opts),
+			    defaultOpts = merge({}, opts);
+
+			return genSauce(fn, optsPosition, defaultOpts, optsRemaining, args);
 		};
 
 		return masala;
--- test/masala-test.js
+++ test/masala-test.js
@@ -4,7 +4,7 @@ var a = require('assert');
 
 describe('masala', function(){
 
-	it('should masala in the I-totally-made-this-up sense, taking the arity from the default options', function(){
+	it('should masala in the I-totally-just-made-this-up sense, taking the arity from the default options', function(){
 		var sum5 = function(o){ return o.a + o.b + o.c + o.d + o.e };
 		var sum5C = masala(sum5, { a: null, b: null, c: null, d: null, e: null });
 
@@ -13,7 +13,25 @@ describe('masala', function(){
 			sum5C({a: 1})({b: 2})({c: 3})({d: 4})({e: 5}));
 	});
 
-	it('should be pure - each new argument should not affect the overall list', function(){
+	it('should masala arguments after the first in the haskell sense, taking the arity from the function', function(){
+		var sum5 = function(o, d, e){ return o.a + o.b + o.c + d + e };
+		var sum5C = masala(sum5, { a: null, b: null, c: null });
+
+		a.equal(
+			sum5({ a: 1, b: 2, c: 3 }, 4, 5 ),
+			sum5C({a: 1}, 4)({b: 2})({c: 3})(5));
+		a.equal(
+			sum5({ a: 1, b: 2, c: 3 }, 4, 5 ),
+			sum5C(4)({a: 1})({b: 2})({c: 3}, 5));
+	});
+
+	it('should drop "extra" arguments', function(){
+		var reportArgs = masala(function(o, a, b){ return [].slice.call(arguments) }, { a: 1 });
+
+		a.deepEqual(reportArgs('a', 'b', 'c', 'd', 'e'), [{ a: 1 }, 'a', 'b']);
+	});
+
+	it('should be pure - each new option should not affect the overall list', function(){
 		var add = masala(function(o){ return o.a + o.b }, { a: null, b: null });
 		var add1 = add({ a: 1 });
 		var add2 = add({ b: 2 });
@@ -28,7 +46,7 @@ describe('masala', function(){
 		a.equal(add2({ a: 4 }), 6);
 	});
 
-	it('should allow multiple arguments to be passed at a time', function(){
+	it('should allow multiple options to be passed at a time', function(){
 		var sum3 = masala(function(o){ return o.a + o.b + o.c }, { a: null, b: null, c: null });
 
 		a.equal(sum3({ a: 1, b: 2, c: 3 }), sum3({ a: 1, b: 2 })({ c: 3 }));
@@ -36,15 +54,15 @@ describe('masala', function(){
 		a.equal(sum3({ a: 1, b: 2, c: 3 }), sum3({ a: 1 })({ b: 2 })({ c: 3 }));
 	});
 
-	it('should allow allow default arguments to be passed', function(){
+	it('should allow allow default options to be passed', function(){
 		var sum3 = masala(function(o){ return o.a + o.b + o.c }, { a: null, b: null, c: 3 });
 
 		a.equal(sum3({ a: 1, b: 2 }), 6);
 	});
 
-	it('should allow allow default arguments to be overridden', function(){
+	it('should allow allow default options to be overridden', function(){
 		var sum3 = masala(function(o){ return o.a + o.b + o.c }, { a: 1, b: null, c: 3 });
 
-		a.equal(sum3({ b: 2 }), sum3({ a: 3, b: 2, c: 1 }));
+		a.equal(sum3({ a: 3, b: 2, c: 3 }), 8);
 	});
 });
